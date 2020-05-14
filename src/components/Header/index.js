7bac2ba96d248981bace26545c1f5cc47390dba8
queirozdaniel <danielqueirozak77@gmail.com>
2020-05-14 02:00:11 -0300

    Criando navegação e páginas dinamicas para imagens

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import style from './style.module.css'
 
-const Header = () => {
+const Header = ({ title = 'Galeria de Imagens' }) => {
     return (
         <div>
-            <h1 className={style.title}>Galeria de Imagens</h1>
-            <p className={style.subTitle}> Imagens de Setups inspiradores</p>
+            <h1 className={style.title}>{title}</h1>
+            <Link to='/'>
+                <p className={style.subTitle}>Setups inspiradores!</p>
+            </Link>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery, Link } from 'gatsby'
 import Image from 'gatsby-image'
 import '../assets/style.css'
 import Header from '../components/Header'
 
 const Index = () => {
 
-    const images = useStaticQuery(graphql`
+  const images = useStaticQuery(graphql`
     query MyQuery {
         allFile {
           edges {
@@ -23,21 +23,23 @@ const Index = () => {
       }
     `)
 
-    return (
-        <div>
-            <Header />
-            <div className='grid max-w-4xl grid-cols-2 gap-4 mx-auto p-6'>
-                {images.allFile.edges.map(image => {
-                    return (
-                        <div className='shadow rounded'>
-                            <Image className='rounded-t' fluid={image.node.childImageSharp.fluid} />
-                            <p className='p-6'>{image.node.relativePath}</p>
-                        </div>
-                    )
-                })}
+  return (
+    <div>
+      <Header />
+      <div className='grid max-w-4xl grid-cols-2 gap-4 mx-auto p-6'>
+        {images.allFile.edges.map(image => {
+          return (
+            <div className='shadow rounded'>
+              <Link to={'/image/' + image.node.relativePath}>
+                <Image className='rounded-t' fluid={image.node.childImageSharp.fluid} />
+                <p className='p-6'>{image.node.relativePath}</p>
+              </Link>
             </div>
-        </div>
-    )
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Index
